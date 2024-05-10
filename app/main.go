323dd0b8b41e9c7558d85a92536925ff006f820f
Outtech105k <techno510tk@gmail.com>
2024-05-10 14:26:59 +0900

    Recover to removed .git

---
+++ app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+	engine.LoadHTMLGlob("templates/*")
+	engine.Static("/static", "./static")
+	engine.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"message": "hello gin",
+		})
+	})
+	engine.Run(":80")
+}
