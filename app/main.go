76388bff7c58ee317d4c19083a428a3871722342
Outtech105k <techno510tk@gmail.com>
2024-05-11 07:26:36 +0000

    Test remote commit from in container.

--- app/main.go
+++ app/main.go
@@ -1,6 +1,5 @@
 package main
 
-<<<<<<< HEAD
 import (
 	"net/http"
 
@@ -10,26 +9,11 @@ import (
 func main() {
 	engine := gin.Default()
 	engine.LoadHTMLGlob("templates/*")
+
 	engine.Static("/static", "./static")
 	engine.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"message": "hello gin",
-		})
+		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
+
 	engine.Run(":80")
-=======
-import "github.com/gin-gonic/gin"
-
-import "net/http"
-
-func main() {
-    engine:= gin.Default()
-    engine.LoadHTMLGlob("templates/*")
-    engine.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.html", gin.H{
-            "message": "hello gin",
-        })
-    })
-    engine.Run(":80")
->>>>>>> origin/main
 }
